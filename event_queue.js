762ca1844f6ee27698ea0b1096933b801e9492b0
Kiriakos Velissariou <kir.velissariou@gmail.com>
2017-06-02 01:27:29 +0300

    Add event_queue file

---
+++ event_queue.js
@@ -0,0 +1,15 @@
+// This file demonstrates event queues
+function waitThreeSeconds() {
+  var ms = 3000 + new Date().getTime();
+  while (new Date() < ms) {}
+  console.log('finished function');
+}
+
+function clickHandler() {
+  console.log('click event!');
+}
+
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
