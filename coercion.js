9dc0e556f6023a8e13c5af5576d2f9f01b31b0da
Kiriakos Velissariou <kir.velissariou@gmail.com>
2017-06-05 19:34:05 +0300

    Add files for objects and properties

---
+++ coercion.js
@@ -0,0 +1,8 @@
+// This file demonstrates when coercion can be useful
+var a;
+
+// Goes to internet and looks for a value
+
+if (a || a === 0) {
+  console.log('Something is there');
+}
---
+++ test.js
@@ -0,0 +1,9 @@
+// This file demonstrates
+function greet(name) {
+  name = name || "Kiriakos";
+  console.log("Hello " + name);
+}
+
+
+greet("Booo");
+greet();
