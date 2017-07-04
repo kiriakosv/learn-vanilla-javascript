bf2d94a6166ca40caa75d2159026aa0a0ca09b90
Kiriakos Velissariou <kir.velissariou@gmail.com>
2017-07-05 01:23:23 +0300

    Add iife example

---
+++ js-files/iife.js
@@ -0,0 +1,25 @@
+// Function statement
+function greet(name) {
+  console.log("Hello " + name);
+};
+greet("John");
+
+// Function expression
+var greetFunc = function(name) {
+  console.log("Hello " + name);
+};
+greetFunc("John");
+
+// Immediately invoked function expression
+var greeting = function(name) {
+  return "Hello " + name;
+}("John");
+
+console.log(greeting);
+
+3;
+
+(function(name) {
+  var greeting = "Hello ";
+  console.log(greeting + name);
+}("John")); // IIFE
