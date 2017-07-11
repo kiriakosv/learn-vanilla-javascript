41b9dd06fc3829f2576193bab4fd2e2fccc420f7
Kiriakos Velissariou <kir.velissariou@gmail.com>
2017-07-12 01:11:50 +0300

    Add new examples

---
+++ js-files/reflection_and_extend.js
@@ -0,0 +1,34 @@
+var person = {
+  firstname: 'Default',
+  lastname: 'Default',
+  getFullName: function () {
+    return this.firstname + ' ' + this.lastname
+  }
+}
+
+var john = {
+  firstname: 'John',
+  lastname: 'Doe'
+}
+
+// don't do this EVER! For demo purposes only
+john.__proto__ = person
+
+for (var prop in john) {
+  if (john.hasOwnProperty(prop)) {
+    console.log(prop + ': ' + john[prop])
+  }
+}
+
+var jane = {
+  address: '111 Main St.',
+  getFormalFullName: function () {
+    return this.lastname + ', ' + this.firstname
+  }
+}
+
+var jim = {
+  getFirstName = function () {
+    return firstname
+  }
+}
